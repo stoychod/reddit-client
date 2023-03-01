b0ec7e923c1df4cbe82dc0c496ab3b9fbdfd35ab
Stoycho Donev <stoychod79@gmail.com>
2023-03-01 17:04:20 +0000

    feat: add styling to Post component

--- src/features/Post/Post.jsx
+++ src/features/Post/Post.jsx
@@ -12,17 +12,26 @@ const Post = () => {
     <div className="post">
       <div className="post-wrapper">
         <div className="post-votes-container">
-          <TiArrowUpOutline />
-          <p>{votesNumber}</p>
-          <TiArrowDownOutline />
+          <button className=" icon-action-button post-vote-up">
+            <TiArrowUpOutline className="icon-action" />
+          </button>
+          <p className="post-votes-number">{votesNumber}</p>
+          <button className="icon-action-button post-vote-down">
+            <TiArrowDownOutline className="icon-action" />
+          </button>
         </div>
         <div className="post-container">
-          <h3>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.</h3>
+          <h3 className="post-title">{title}</h3>
           <div className="post-image-container">
             <img src="../../../assets/0tg7wbrj5uka1.jpg" className="post-image" />
           </div>
           <div className="post-footer">
-            <span className="post-comments"><TiMessage />{commentsCount}</span>
+            <span className="post-comments-container">
+              <button className="icon-action-button">
+                <TiMessage className="icon-action" />
+              </button>
+              {commentsCount}
+            </span>
             <span className="author-name">{author}</span>
           </div>
         </div>
