2e11f0eebcb000bcd730888bea1dc3bec84e541d
Stoycho Donev <stoychod79@gmail.com>
2023-03-07 19:27:37 +0000

    feat: add getComments query to apiSlice

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -8,7 +8,11 @@ export const apiSlice = createApi({
       query: (subReddit) => `/r/${subReddit}.json`,
       transformResponse: (response) => response.data.children,
     }),
+    getComments: builder.query({
+      query: (permalink) => `${permalink}.json`,
+      transformResponse: (response) => response[1].data.children,
+    }),
   }),
 });
 
-export const { useGetPostsQuery } = apiSlice;
+export const { useGetPostsQuery, useGetCommentsQuery } = apiSlice;
