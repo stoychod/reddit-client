85f3e858fee2da7a2df615d84b2d849cc8b95a0a
Stoycho Donev <stoychod79@gmail.com>
2023-03-08 06:18:56 +0000

    feat: add a comment component and react-markdown package

---
+++ src/features/Comment/Comment.jsx
@@ -0,0 +1,15 @@
+import ReactMarkdown from "react-markdown";
+
+const Comment = ({ comment }) => {
+  return (
+    <div className="comment">
+      <div className="comment-metadata">
+        <p className="comment-author">{comment.author}</p>
+        <p className="comment-created-time"></p>
+      </div>
+      <ReactMarkdown children={comment.body} />
+    </div>
+  );
+};
+
+export default Comment;
