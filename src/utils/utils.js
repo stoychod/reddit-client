1be58d4ae5daa29c80cc28042180121ffc834e82
Stoycho Donev <stoychod79@gmail.com>
2023-03-10 16:42:55 +0000

    fix: modify formatNumber util function to single number of precision

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -16,12 +16,12 @@ export const formatNumber = (num) => {
     return num;
   }
   let si = [
-    { v: 1e3, s: "K" },
-    { v: 1e6, s: "M" },
-    { v: 1e9, s: "B" },
-    { v: 1e12, s: "T" },
-    { v: 1e15, s: "P" },
-    { v: 1e18, s: "E" },
+    { v: 1e3, s: "k" },
+    { v: 1e6, s: "m" },
+    // { v: 1e9, s: "B" },
+    // { v: 1e12, s: "T" },
+    // { v: 1e15, s: "P" },
+    // { v: 1e18, s: "E" },
   ];
   let index;
   for (index = si.length - 1; index > 0; index--) {
@@ -30,7 +30,7 @@ export const formatNumber = (num) => {
     }
   }
   return (
-    (num / si[index].v).toFixed(2).replace(/\.0+$|(\.[0-9]*[1-9])0+$/, "$1") +
+    (num / si[index].v).toFixed(1).replace(/\.0+$|(\.[0-9]*[1-9])0+$/, "$1") +
     si[index].s
   );
 };
