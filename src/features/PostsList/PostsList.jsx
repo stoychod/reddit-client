3d10fbf5bbb3c556acee26a6bff451afd5a17c2e
Stoycho Donev <stoychod79@gmail.com>
2023-03-12 17:22:33 +0000

    feat: add filter posts by title functionality

--- src/features/PostsList/PostsList.jsx
+++ src/features/PostsList/PostsList.jsx
@@ -1,27 +1,40 @@
-import Post from '../Post/Post'
-import { useGetPostsQuery } from '../api/apiSlice'
+import Post from "../Post/Post";
+import { useGetPostsQuery } from "../api/apiSlice";
+import { useSelector } from "react-redux";
+import { selectSearchTerm } from "../Header/searchTermSlice";
 
 export const PostsList = () => {
-  const {
+  let {
     data: posts,
     isLoading,
     isSuccess,
     isError,
     error,
-  } = useGetPostsQuery("home");
+  } = useGetPostsQuery("pics");
+
+  const searchTerm = useSelector(selectSearchTerm);
+
+  const filterPosts = (posts, searchTerm) => {
+    return posts.filter((post) => {
+      return post.data.title.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+  };
 
   let content;
 
   if (isLoading) {
     content = "Loading...";
   } else if (isSuccess) {
+    if (searchTerm !== "") {
+      posts = filterPosts(posts, searchTerm);
+    }
     content = posts.map((post) => <Post key={post.data.id} post={post.data} />);
     // console.log(posts);
     // posts.forEach(element => {
     //   console.log(element.data)
     // });
   } else if (isError) {
-    content = <div>{error.toString()}</div>
+    content = <div>{error.toString()}</div>;
   }
 
   // return <Post />;
