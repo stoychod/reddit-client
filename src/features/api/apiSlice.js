4b2fadd3dc5fb036c33f9db3c10738b1772279ae
Stoycho Donev <stoychod79@gmail.com>
2023-03-13 11:07:13 +0000

    fix: remove leading slash in apiSlice query functions
    changes made earlier actually broke the api

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -9,11 +9,11 @@ export const apiSlice = createApi({
       transformResponse: (response) => response.data.children,
     }),
     getComments: builder.query({
-      query: (permalink) => `/${permalink}.json`,
+      query: (permalink) => `${permalink}.json`,
       transformResponse: (response) => response[1].data.children,
     }),
     getSubreddits: builder.query({
-      query: () => "/subreddits.json",
+      query: () => "subreddits.json",
       transformResponse: (response) => response.data.children,
     }),
   }),
