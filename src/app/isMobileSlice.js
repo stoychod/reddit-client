6d4e983e05fb57d8e9b0929e9872fe794bac6e30
Stoycho Donev <stoychod79@gmail.com>
2023-03-17 14:57:34 +0000

    refactor: change isMobile reducer implementation
    hard-code mobile window width, calculate initial state, expect the
    payload to be a number representing current window width

--- src/app/isMobileSlice.js
+++ src/app/isMobileSlice.js
@@ -1,10 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const mobileWindowWidth = 680;
+const initialState = window.innerWidth <= mobileWindowWidth;
+
 export const isMobileSlice = createSlice({
   name: "isMobile",
-  initialState: false,
+  initialState,
   reducers: {
-    setIsMobile: (state, action) => action.payload,
+    setIsMobile: (state, action) => {
+      // ecxpect the payload to be a number
+      // representing the current window width
+      return action.payload <= mobileWindowWidth;
+    },
   },
 });
 
