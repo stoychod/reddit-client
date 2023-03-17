7c798a9a605e1f7c825f70e6c86c940dd7aca7bf
Stoycho Donev <stoychod79@gmail.com>
2023-03-17 15:41:01 +0000

    feat: add a resize window event listener to control isMobile slice

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { useEffect } from "react";
 import "./App.css";
 import Header from "./features/Header/Header";
 import PostsList from "./features/PostsList/PostsList";
 import SubredditsList from "./features/SubredditsList/SubredditsList";
+import { setIsMobile } from "./app/isMobileSlice";
+import { useDispatch } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+
+  const handleWindowResize = () => {
+    dispatch(setIsMobile(window.innerWidth));
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowResize);
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
   return (
     <>
       <header>
