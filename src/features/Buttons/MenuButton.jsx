885f80c42d73728280426a428300be328866f663
Stoycho Donev <stoychod79@gmail.com>
2023-03-19 08:09:26 +0000

    feat: add an onClick handler function to the menu button

--- src/features/Buttons/MenuButton.jsx
+++ src/features/Buttons/MenuButton.jsx
@@ -1,12 +1,22 @@
 import { GrMenu, GrClose } from "react-icons/gr";
-import { useSelector } from "react-redux";
-import { selectSdiebarVisible } from "../../app/sidebarVisible";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  selectSdiebarVisible,
+  setSidedbarVisible,
+} from "../../app/sidebarVisible";
 
 const MenuButton = () => {
   const sidebarVisible = useSelector(selectSdiebarVisible);
+  const dispatch = useDispatch();
+
+  const handleOnClick = () => dispatch(setSidedbarVisible(!sidebarVisible));
 
   return (
-    <button type="button" className="icon-action-button">
+    <button
+      type="button"
+      className="icon-action-button"
+      onClick={handleOnClick}
+    >
       {sidebarVisible ? (
         <GrClose className="icon-action-button" />
       ) : (
