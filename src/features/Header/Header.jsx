47111a415f6bd2037ef6c3a8114a53942247b9c6
Stoycho Donev <stoychod79@gmail.com>
2023-03-23 08:13:48 +0000

    refactor: cleanup Header.jsx

--- src/features/Header/Header.jsx
+++ src/features/Header/Header.jsx
@@ -2,13 +2,12 @@ import { BsReddit } from "react-icons/bs";
 import "./Header.css";
 import MenuButton from "../Buttons/MenuButton";
 import { useSelector, useDispatch } from "react-redux";
-import { setSearchTerm, selectSearchTerm } from "../../app/searchTermSlice";
-import isMobileSlice, { selectIsMobile } from "../../app/isMobileSlice";
+import { setSearchTerm } from "../../app/searchTermSlice";
+import { selectIsMobile } from "../../app/isMobileSlice";
 import { debounce } from "../../utils/utils";
 
 const Header = () => {
   const isMobile = useSelector(selectIsMobile);
-  // const searchTerm = useSelector(selectSearchTerm);
   const dispatch = useDispatch();
 
   const handleOnChange = debounce((value) => {
@@ -26,7 +25,6 @@ const Header = () => {
           onChange={({ target }) => handleOnChange(target.value)}
           type="text"
           placeholder="Search posts"
-          // value={searchTerm}
         />
       </form>
       <div className="menu-button">{isMobile && <MenuButton />}</div>
